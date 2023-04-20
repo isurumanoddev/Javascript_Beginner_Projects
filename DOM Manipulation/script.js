39188770ee2a60820017cb2422ce422c043b445d
isurumanod <isurumanod99@gmail.com>
2023-04-20 19:47:22 +0530

    create logic

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -1,1 +1,27 @@
+let greenSquare = document.getElementById("green");
+let purpleSquare = document.getElementById("purple");
+let orangeSquare = document.getElementById("orange");
+let clearButton = document.getElementById("clear");
 
+const sqaures = document.querySelectorAll(".colorSquares");
+
+const clickTimes = { purple: 0, green:0, orange: 0}
+sqaures.forEach(square => {
+
+    square.onclick = () => {
+        console.log(square.value);
+        clickTimes[square.value] += 1;
+        square.innerText = clickTimes[square.value]
+    };
+
+});
+
+clearButton.onclick = () => clearScore()
+
+function clearScore() {
+    console.log("clear")
+    sqaures.forEach(square => {
+        square.innerHTML = ""
+        clickTimes[square.value] = 0;
+    });
+}
