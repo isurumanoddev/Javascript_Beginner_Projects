6a1071d51787ba1325e00a12bf7fb73d6fe4a8fb
isurumanod <isurumanod99@gmail.com>
2023-04-21 11:53:26 +0530

    ui complete

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -3,7 +3,7 @@ let purpleSquare = document.getElementById("purple");
 let orangeSquare = document.getElementById("orange");
 let clearButton = document.getElementById("clear");
 
-const squares = document.querySelectorAll(".colorSquares");
+const squares = document.querySelectorAll(".rpsButton");
 
 const timesClicked = { "green":0,"purple":0,"orange":0,}
 squares.forEach(square => {
