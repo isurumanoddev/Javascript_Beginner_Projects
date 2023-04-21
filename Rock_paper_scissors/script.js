f8f9d682a06638a884a13439eb448f9b5b7507c2
isurumanod <isurumanod99@gmail.com>
2023-04-21 11:41:59 +0530

    ui update

---
+++ Rock_paper_scissors/script.js
@@ -0,0 +1,25 @@
+let greenSquare = document.getElementById("green");
+let purpleSquare = document.getElementById("purple");
+let orangeSquare = document.getElementById("orange");
+let clearButton = document.getElementById("clear");
+
+const squares = document.querySelectorAll(".colorSquares");
+
+const timesClicked = { "green":0,"purple":0,"orange":0,}
+squares.forEach(square => {
+
+    square.onclick = () => {
+        console.log(square.value);
+        timesClicked[square.value] += 1;
+        square.innerText = timesClicked[square.value]
+    }
+});
+
+clearButton.onclick = () => {
+    squares.forEach(square =>{
+
+        timesClicked[square.value] = 0;
+        square.innerText = ""
+
+    })
+};
