3ffb163fda592eb7dcbc70c0ac29bbf8a146e5d6
isurumanod <isurumanod99@gmail.com>
2023-04-24 10:51:44 +0530

    whether app complete

--- Whether_app/script.js
+++ Whether_app/script.js
@@ -1,15 +1,5 @@
-// const cityWhether = document.getElementById("cityName");
-// const cloud = document.getElementById("cloud");
-// const country = document.getElementById("country");
-// const temp = document.getElementById("temp");
-// const wind = document.getElementById("wind");
-// const humidity = document.getElementById("humidity");
 const whetherInput = document.getElementById("whetherInput");
-// const searchInput = document.getElementById("city")
-// let data = {City: null, Country: "", Temp: "", Wind_Speed: "", Humidity: "", Cloud: "",}
 
-
-// const searchButton = document.getElementById("Button");
 
 const searchCity = async () => {
     const cityName = whetherInput.value;
@@ -36,14 +26,7 @@ const getWhether = async (city) => {
         const response = await fetch(url, options);
         const result = await response.json();
         showWhetherData(result)
-        // console.log(result)
 
-        // console.log("City :", result.location["name"]);
-        // console.log("Country :", result.location["country"]);
-        // console.log("Temp :", result.current["temp_c"]);
-        // console.log("Wind Speed :", result.current["wind_kph"]);
-        // console.log("Humidity: :", result.current["humidity"]);
-        // console.log("Cloud: :", result["current"]["condition"].text);
 
 
     } catch (error) {
@@ -65,18 +48,3 @@ const showWhetherData = (whetherData) => {
 
 };
 
-
-// cityWhether.innerText = result[0]
-// cityWhether.innerText = "kegalle"
-// cloud.innerText = result["current"]["condition"].text
-// country.innerText = result.location["country"]
-// temp.innerText = result.current["temp_c"]
-// wind.innerText = result.current["wind_kph"]
-// humidity.innerText = result.current["humidity"]
-
-// data.City = result.location["name"]
-// data.Country = result.location["country"]
-// data.Temp = result.current["temp_c"]
-// data.Wind_Speed = result.current["wind_kph"]
-// data.Humidity = result.current["humidity"]
-// data.Cloud = result["current"]["condition"].tex
